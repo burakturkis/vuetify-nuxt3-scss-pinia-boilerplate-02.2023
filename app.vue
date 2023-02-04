912a566bae35160436b19a240e8d416cfae093f4
Burak Turkis <burakturkis@neisterse.com>
2023-02-04 03:58:15 +0300

    nuxt3 page and layout transition bug temporary fix.

--- app.vue
+++ app.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+  <Transition name="layout" mode="out-in">
+    <div :key="$route.meta.layout">
+      <NuxtLayout>
+        <Transition name="page" mode="out-in">
+          <div :key="$route.fullPath">
+            <NuxtPage />
+          </div>
+        </Transition>
+      </NuxtLayout>
+    </div>
+  </Transition>
 </template>
 <script lang="ts" setup>
 useHead({
   // as a string,
   // where `%s` is replaced with the title
+  // of the current page
+  title: "Welcome ",
   titleTemplate: "%s - bT Boilerplate 2023",
 });
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' },
-    layoutTransition: { name: 'layout', mode: 'out-in' },
-  },
   css: [
     'vuetify/lib/styles/main.sass',
     '@mdi/font/css/materialdesignicons.min.css',
